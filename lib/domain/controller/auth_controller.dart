6b4089dcab81da26c55059c27b605cd09c095e9a
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2022-11-21 21:47:54 +0700

    http state

--- lib/domain/controller/auth_controller.dart
+++ lib/domain/controller/auth_controller.dart
@@ -5,16 +5,32 @@ import 'package:booking_app/data/endpoint/endpoint.dart';
 import 'package:booking_app/domain/repository/repository.dart';
 
 class AuthController implements AuthRepository {
+  AuthController(this._httpState);
+
   final ApiClient _apiClient = ApiClient();
+  final HttpState _httpState;
 
   @override
   Future<BaseResponse> forgotPassword(String email) async {
+    const String url = Endpoint.forgotPassword;
+    const String method = 'POST';
+    _httpState.onStartRequest(url, method);
+
     late final BaseResponse baseResponse;
-    final response = await _apiClient
-        .post(Uri.parse(Endpoint.forgotPassword), body: {'email': email});
+
+    final response =
+        await _apiClient.post(Uri.parse(url), body: {'email': email});
+    _httpState.onEndRequest(url, method);
+
     if (response.statusCode < 500) {
+      if (response.statusCode > 199 && response.statusCode < 300) {
+        _httpState.onSuccessRequest(url, method);
+      } else {
+        _httpState.onErrorRequest(url, method);
+      }
       baseResponse = BaseResponse.fromJson(jsonDecode(response.body));
     } else {
+      _httpState.onErrorRequest(url, method);
       baseResponse = BaseResponse(message: response.body);
     }
     return baseResponse;
@@ -22,12 +38,24 @@ class AuthController implements AuthRepository {
 
   @override
   Future<BaseResponse> login(String email, String password) async {
+    const String url = Endpoint.login;
+    const String method = 'POST';
+    _httpState.onStartRequest(url, method);
+
     late final BaseResponse baseResponse;
-    final response = await _apiClient.post(Uri.parse(Endpoint.login),
-        body: {'email': email, 'password': password});
+    final response = await _apiClient
+        .post(Uri.parse(url), body: {'email': email, 'password': password});
+    _httpState.onEndRequest(url, method);
+
     if (response.statusCode < 500) {
+      if (response.statusCode > 199 && response.statusCode < 300) {
+        _httpState.onSuccessRequest(url, method);
+      } else {
+        _httpState.onErrorRequest(url, method);
+      }
       baseResponse = BaseResponse.fromJson(jsonDecode(response.body));
     } else {
+      _httpState.onErrorRequest(url, method);
       baseResponse = BaseResponse(message: response.body);
     }
     return baseResponse;
@@ -35,18 +63,40 @@ class AuthController implements AuthRepository {
 
   @override
   Future<void> logout() async {
-    await _apiClient.post(Uri.parse(Endpoint.logout));
+    const String url = Endpoint.logout;
+    const String method = 'POST';
+    _httpState.onStartRequest(url, method);
+
+    await _apiClient.post(Uri.parse(url)).then((value) {
+      _httpState.onSuccessRequest(url, method);
+    }).catchError((onError) {
+      _httpState.onErrorRequest(url, method);
+    });
+
+    _httpState.onEndRequest(url, method);
   }
 
   @override
   Future<BaseResponse> register(
       String email, String password, String fullName) async {
+    const String url = Endpoint.register;
+    const String method = 'POST';
+    _httpState.onStartRequest(url, method);
+
     late final BaseResponse baseResponse;
-    final response = await _apiClient.post(Uri.parse(Endpoint.register),
+    final response = await _apiClient.post(Uri.parse(url),
         body: {'email': email, 'password': password, 'full_name': fullName});
+    _httpState.onEndRequest(url, method);
+
     if (response.statusCode < 500) {
+      if (response.statusCode > 199 && response.statusCode < 300) {
+        _httpState.onSuccessRequest(url, method);
+      } else {
+        _httpState.onErrorRequest(url, method);
+      }
       baseResponse = BaseResponse.fromJson(jsonDecode(response.body));
     } else {
+      _httpState.onErrorRequest(url, method);
       baseResponse = BaseResponse(message: response.body);
     }
     return baseResponse;
